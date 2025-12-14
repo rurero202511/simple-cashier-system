19eed14af6d3aa5e10745b99da19cacae465cf86
rurero <rurero@proton.me>
2025-12-14 19:15:05 +0900

    SCS-001 最小構成の設定追加 (#1)

---
+++ src/main/java/io/github/rurero202511/simplecashiersystem/Application.java
@@ -0,0 +1,12 @@
+package io.github.rurero202511.simplecashiersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
